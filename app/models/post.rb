e11888441b03abc8271af575ec8ad8455eab6d61
rmdashrfv <rmdashrfv@hey.com>
2022-12-20 12:13:58 -0500

    added serializer

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   validates :content, presence: true
   belongs_to :user
   has_many :comments
+  # delegate these methods to this object
+  delegate :username, :email, to: :user
+
+  # def username
+  #   self.user.username
+  # end
 end
